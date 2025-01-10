f8244825b40630d2959578eba46e32c71d071f5e
Sunay <119887986+suzzy-dszy@users.noreply.github.com>
2025-01-10 21:01:28 +0300

    Desktop Design Finalised
    -Responsiveness implemented.
    -Share pop-up created.
    -Button invert colour implemented successfully.
    -Desktop layout achieved.

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,7 +1,14 @@
 var authorSection = document.querySelector('.card__content--author');
 var shareSection = document.querySelector('.card__content--share-menu');
+var shareButton1 = document.querySelector('.card__content--share-button-1');
+var shareButtonIcon1 = document.querySelector('.card__content--share-icon-1');
 
 function toggleShareMenu() {
   authorSection.classList.toggle('active');
   shareSection.classList.toggle('active');
+}
+
+function invertBtnColor() {
+  shareButton1.classList.toggle('invert-btn-colour');
+  shareButtonIcon1.classList.toggle('invert-icon-colour');
 }
